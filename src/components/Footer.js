ba63771c39f76a9abd0b57acea74deeb5d866b9d
osamaElzero <o@gmail.com>
2024-12-09 17:52:21 +0200

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="footer  flex flex-col justify-center items-center gap-3 bg-red-200 py-10 text-gray-600 ">
+      <h2 className="brand text-4xl font-semibold text-center lowercase italic ">
+        Food<span className=" text-red-500 ">ac</span>
+      </h2>
+      <p className="text-gray-700">&copy; 2022 foodac. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
